2da8bf78b6e1abda563747249637013794a385ad
Bozdogan <boraozdogan99@gmail.com>
2022-06-06 23:42:41 +0300

    INITIAL COMMIT

---
+++ src/main/java/org/bozdgn/qdemo/QdemoApplication.java
@@ -0,0 +1,13 @@
+package org.bozdgn.qdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QdemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QdemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/bozdgn/qdemo/QdemoApplicationTests.java
@@ -0,0 +1,13 @@
+package org.bozdgn.qdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QdemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
